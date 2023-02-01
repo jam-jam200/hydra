546aeb395ea2182d13119973f6ae61a1cb8e5d6f
jam-jam200 <jessieoyaks4j@gmail.com>
2023-02-01 16:16:50 +0100

    constants

---
+++ src/constants/index.js
@@ -0,0 +1,41 @@
+export const contact = [
+  {
+    id: "location",
+    imgUrl: "/location.png",
+    title: "Pay Us a Visit",
+    subtitle: "Union St, Seattle, WA 98101, United States",
+  },
+  {
+    id: "call",
+    imgUrl: "/phone.png",
+    title: "Give Us a Call",
+    subtitle: "(110) 1111-1010",
+  },
+  {
+    id: "message",
+    imgUrl: "/mail.png",
+    title: "Send Us a Message",
+    subtitle: "Contact@HydraVTech.com",
+  },
+];
+
+export const services = [
+  {
+    id: "location",
+    imgUrl: "/location.png",
+    title: "Pay Us a Visit",
+    subtitle: "Union St, Seattle, WA 98101, United States",
+  },
+  {
+    id: "call",
+    imgUrl: "/phone.png",
+    title: "Give Us a Call",
+    subtitle: "(110) 1111-1010",
+  },
+  {
+    id: "message",
+    imgUrl: "/mail.png",
+    title: "Send Us a Message",
+    subtitle: "Contact@HydraVTech.com",
+  },
+];
