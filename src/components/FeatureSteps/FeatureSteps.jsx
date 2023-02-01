fda6d0e3e740c959578418ee2424ffbfb1fe30ee
jam-jam200 <jessieoyaks4j@gmail.com>
2023-02-01 16:49:00 +0100

    creating components

--- src/components/FeatureSteps/FeatureSteps.jsx
+++ src/components/FeatureSteps/FeatureSteps.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FeatureSteps = () => {
   return (
     <div>
-      
+      feature steps
     </div>
   )
 }
--- src/components/Midsection/Midsection.jsx
+++ src/components/Midsection/Midsection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Midsection = () => {
   return (
     <div>
-      
+      mid section
     </div>
   )
 }
