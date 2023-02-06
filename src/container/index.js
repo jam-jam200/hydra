4cc3bfc30d1ab479839872bf56a0a96160991562
jam-jam200 <jessieoyaks4j@gmail.com>
2023-02-06 17:07:25 +0100

    implementation stage

---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./About/About";
+export { default as Contact } from "./Contact/Contact";
+export { default as Footer } from "./Footer/Footer";
+export { default as Header } from "./Header/Header";
+export { default as Technology } from "./Technology/Technology";
+export { default as WHydra } from "./WHydra/WHydra";
