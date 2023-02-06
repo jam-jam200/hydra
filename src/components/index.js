674fc04c2bda392a90557f366b901627ce158e5f
jam-jam200 <jessieoyaks4j@gmail.com>
2023-02-06 17:05:05 +0100

    implementation stage

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Brand } from "./Brand/Brand";
+export { default as FeatureSteps } from "./FeatureSteps/FeatureSteps";
+export { default as HeaderSteps } from "./HeaderSteps/HeaderSteps";
+export { default as Hydra } from "./Hydra/Hydra";
+export { default as Midsection } from "./Midsection/Midsection";
