034762839963731ef8433a8e8cbb2c1d42371587
jam-jam200 <jessieoyaks4j@gmail.com>
2023-02-08 14:53:52 +0100

    starting with the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <div className="app">
-     <img src="/hydraDesktop.png" alt="hydrabg" />
+      <img src="/hydraDesktop.png" alt="hydrabg" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
